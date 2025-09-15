3c768690603fbe8080dfb4f153055aab19419582
amirholya01 <amirholya01@gmail.com>
2025-09-15 07:10:33 +0330

    Checking AuthController

--- src/modules/auth/Auth.Controller.js
+++ src/modules/auth/Auth.Controller.js
@@ -1,25 +1,25 @@
-const autoBind = require('auto-bind');
-const AuthService = require('./Auth.Service');
-const AuthMessage = require('./Auth.Message');
+const autoBind = require("auto-bind");
+const AuthService = require("./Auth.Service");
+const AuthMessage = require("./Auth.Message");
 
-class AuthController{
-    #service;
-    constructor(){
-        autoBind(this);
-        this.#service = AuthService;
-    }
+class AuthController {
+  #service;
+  constructor() {
+    autoBind(this);
+    this.#service = AuthService;
+  }
 
-    async sendOtp(req, res, next){
-        try {
-            const {mobile} = req.body;
-            await this.#service.sendOtp(mobile);
-            return res.json({
-                message: AuthMessage.SendOtpSuccessfully
-            });
-        } catch (error) {
-            next(error);
-        }
+  async sendOtp(req, res, next) {
+    try {
+      const { mobile } = req.body;
+      await this.#service.sendOtp(mobile);
+      return res.json({
+        message: AuthMessage.SendOtpSuccessfully,
+      });
+    } catch (error) {
+      next(error);
     }
+  }
 }
 
-module.exports = new AuthController()
+module.exports = new AuthController();
