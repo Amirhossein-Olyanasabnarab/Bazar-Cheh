a0e6e1fbd976c820070ad74e22c6d5512de7e1aa
amirholya01 <amirholya01@gmail.com>
2025-09-15 06:50:13 +0330

    AuthController

---
+++ src/modules/auth/Auth.Controller.js
@@ -0,0 +1,23 @@
+const autoBind = require('auto-bind');
+const AuthService = require('./Auth.Service');
+const AuthMessage = require('./Auth.Message');
+
+class AuthController{
+    #service;
+    constructor(){
+        autoBind(this);
+        this.#service = AuthService;
+    }
+
+    async sendOtp(req, res, next){
+        try {
+            const {mobile} = req.body;
+            await this.#service.sendOtp(mobile);
+            return res.json({
+                message: AuthMessage.SendOtpSuccessfully
+            });
+        } catch (error) {
+            next(error);
+        }
+    }
+}
--- src/modules/auth/Auth.Message.js
+++ src/modules/auth/Auth.Message.js
@@ -1,5 +1,6 @@
 const AuthMessage = {
-    OtpCodeNotExpired: "otp code not expired please try later"
+    OtpCodeNotExpired: "otp code not expired please try later",
+    SendOtpSuccessfully: "Sending OTP was successfully"
 }
 
 module.exports = AuthMessage;
