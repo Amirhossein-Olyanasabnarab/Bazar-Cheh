aedcd281c1bb956e1721da1880a4c23b6c9bd96b
amirholya01 <amirholya01@gmail.com>
2025-09-15 07:26:43 +0330

    using errorHandler in main.js

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const express = require('express');
 require('dotenv').config();
 const mainRouter = require('./src/app.routes');
+const notFoundHandler = require('./src/common/exceptions/not-found.handler');
+const allExceptionHandler = require('./src/common/exceptions/all-exception.handler');
+
 function main(){
     const app = express();
 
@@ -10,6 +13,8 @@ function main(){
     app.use(express.json());
     app.use(express.urlencoded({extended: true}));
     app.use(mainRouter);
+    notFoundHandler(app);
+    allExceptionHandler(app);
     app.listen(port, () => {
         console.log(`Server is running on http://localhost:${port}`);
     });
